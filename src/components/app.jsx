36dd2370226afc6769c6aad220fa04cce7cc1c9a
Kevin Nguyen <kngwin9@gmail.com>
2020-01-27 19:38:35 -0800

    saving

---
+++ src/components/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ToDoList from './to-do-list';
+
+function App(props) {
+  return (
+    <div>
+      <h1>To Do List</h1>
+      <ToDoList />
+    </div>
+  );
+}
+
+export default App;
